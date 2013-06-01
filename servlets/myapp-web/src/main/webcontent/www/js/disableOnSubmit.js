753eb29488758ed0cb78261721187f9dd7d2c341
se-yoshi <se.yoshi@gmail.com>
2013-06-01 20:20:25 +0900

    ・URL Rewritingを無効化 ・セッションタイムアウト画面を追加 ・二重送信制御JavaScriptを修正

--- servlets/myapp-web/src/main/webcontent/www/js/disableOnSubmit.js
+++ servlets/myapp-web/src/main/webcontent/www/js/disableOnSubmit.js
@@ -9,11 +9,11 @@ $.fn.disableOnSubmit = function(disableList) {
 	}
 
 	$(this).find(list).each(function() {
-		$(this).removeAttr("disabled");
 		if ($(this).attr("name") != undefined) {
 			$(this).click(function() {
+				$("#disable-on-submit-hidden").remove();
 				$(this).closest("form").append(
-					$("<input type=\"hidden\">")
+					$("<input type=\"hidden\" id=\"disable-on-submit-hidden\">")
 					.attr("name", ($(this).attr("name")))
 					.attr("value", ($(this).attr("value")))
 				);
@@ -22,8 +22,30 @@ $.fn.disableOnSubmit = function(disableList) {
 	});
 
 	$(this).submit(function() {
-		$(this).find(list).attr("disabled", "disabled");
+		$(this).find(list).each(function() {
+			var target = $(this);
+			var isDisabled = target.prop("disabled");
+			target.attr("disabled", "disabled");
+			$(window).unload(function() {
+				target.prop("disabled", isDisabled);
+			});
+		});
 	});
 
 	return this;
+};
+
+$.fn.resetOnSubmit = function(disableList) {
+
+	var list;
+
+	if (disableList == null) {
+		list = "input[type=submit],input[type=button],input[type=reset],button";
+	} else {
+		list = disableList;
+	}
+
+	$(this).find(list).each(function() {
+		$(this).removeAttr("disabled");
+	});
 };
